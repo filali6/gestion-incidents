67f188fb3c48905893730a06429c956a2c28896e
chaima filali <chaimafilali6@gmail.com>
2025-11-29 14:06:44 +0100

    first commit

---
+++ src/main/java/com/ville/intelligente/gestionincidents/GestionIncidentsVilleIntelligenteApplication.java
@@ -0,0 +1,13 @@
+package com.ville.intelligente.gestionincidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionIncidentsVilleIntelligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionIncidentsVilleIntelligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ville/intelligente/gestionincidents/GestionIncidentsVilleIntelligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ville.intelligente.gestionincidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionIncidentsVilleIntelligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
